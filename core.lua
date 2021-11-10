82ee0c6058d71251afca0f237ca6bc797a00c08c
gonzoinc <git@gonzoinc.com>
2021-11-10 09:29:28 -0500

    Incrementing version number

--- core.lua
+++ core.lua
@@ -1,7 +1,7 @@
 Disenchanter = LibStub("AceAddon-3.0"):NewAddon("Disenchanter", "AceEvent-3.0");
 
 local name = "Disenchanter";
-local version = "v1.0";
+local version = "v1.0.1";
 local itemID
 local expacID
 local iType
